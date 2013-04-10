f12f3e390271c765f5aea01b315de34bdddca098
bluepeas <calvin.cam.ucla@gmail.com>
2013-04-09 19:46:14 -0700

    Updated to remove 'unused' parameter warning.

--- execute-command.c
+++ execute-command.c
@@ -8,6 +8,9 @@
 /* FIXME: You may need to add #include directives, macro definitions,
    static function definitions, etc.  */
 
+/* Temporary usage of printf. */
+#include <stdio.h>
+
 int
 command_status (command_t c)
 {
@@ -21,4 +24,6 @@ execute_command (command_t c, bool time_travel)
      add auxiliary functions and otherwise modify the source code.
      You can also use external functions defined in the GNU C Library.  */
   error (1, 0, "command execution not yet implemented");
+  time_travel = false;
+  printf("Type: %i\n", c->type);
 }
