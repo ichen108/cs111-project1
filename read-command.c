a9373eb0b6e383a3f3905afab6e297f76d264345
bluepeas <calvin.cam.ucla@gmail.com>
2013-04-11 18:37:24 -0700

    Functional tokenizer

--- read-command.c
+++ read-command.c
@@ -23,15 +23,7 @@
 /* Global Variables. */
 int line = 0;
 
-/* Defined 'struct command_stream'. */
-typedef struct command_stream
-{
-  void *stream;
-  int (*get_next_byte) (void*);
-  int has_char_waiting;
-  char ungotten_char;
-};
-
+/* Enumeration for token types before being converted to commands. */
 enum token_type 
 {
   WORD,			// 'a-z', 'A-Z', '0-9', '!', '%', '+', ',', '-', '.', '/', ':', '@', '^', '_' 
@@ -45,18 +37,23 @@ enum token_type
   REDIRECT_IN,		// '<'
   REDIRECT_OUT,		// '>'
   END_FILE,		// EOF
-
-  SPACE,		// space
-  SPECIAL_TOKEN,	// ';', '|', '&&', '||', '(', ')', '<', '>', ' '\n' '
-  UNSUPPORTED,		// unsupported token
 };
 
+/* Defined 'struct TOKEN'. */
 typedef struct TOKEN
 {
   enum token_type type;
   char *token_name;
 } *token_t;
 
+/* Defined 'struct command_stream'. */
+struct command_stream
+{
+  void *stream;
+  int (*byte_function) (void*);
+  int has_char_waiting;
+  token_t ctoken;
+};
 
 command_stream_t
 make_command_stream (int (*get_next_byte) (void *),
@@ -66,10 +63,13 @@ make_command_stream (int (*get_next_byte) (void *),
      add auxiliary functions and otherwise modify the source code.
      You can also use external functions defined in the GNU C Library.  */
   //error (1, 0, "command reading not yet implemented");
+
+  /* Initialize stream. */
   command_stream_t curr_stream = checked_malloc(sizeof(struct command_stream));
+  curr_stream->byte_function = get_next_byte;
   curr_stream->stream = get_next_byte_argument;
   curr_stream->has_char_waiting = -1;
-  curr_stream->ungotten_char = '\0';
+  curr_stream->ctoken = NULL;
  
   return curr_stream;
 }
@@ -79,17 +79,25 @@ read_command_stream (command_stream_t s)
 {
   /* FIXME: Replace this with your implementation too.  */
   error (1, 0, "command reading not yet implemented");
+  
+  /* Temporary usage of s. */
+  printf("%d", s->has_char_waiting);
   return 0;
 }
 
-/* Start implementation of TOKENIZER */
 
-/* Checks if there is a byte taken from get_next_byte() and uses it if available. */
-int get_check(command_stream_t s)
+/*************************************************
+ *************************************************
+ ******* Start of TOKENIZER Implementation *******
+ *************************************************
+ *************************************************/
+
+/* Checks if there is a byte taken from a previous call and uses it before moving on. */
+int get(command_stream_t s)
 {
   if(s->has_char_waiting == -1)
   {
-    return s->get_next_byte(s->stream);
+    return s->byte_function(s->stream);
   }
   else
   {
@@ -99,8 +107,8 @@ int get_check(command_stream_t s)
   }
 }
 
-/* Checks if there is a byte and returns it back into the stream due to overlooking. */
-int unget_check(command_stream_t s, int c)
+/* Checks if there is a byte and puts it back into the stream due to overlooking. */
+int put(command_stream_t s, int c)
 {
   if(s->has_char_waiting != -1)
   {
@@ -113,11 +121,39 @@ int unget_check(command_stream_t s, int c)
   }
 }
 
+/* Gets current token. */
+token_t get_token(command_stream_t s) 
+{
+  if (!s->ctoken) 
+  {
+    return get_next_token(s);
+  }
+  else 
+  {
+    token_t ret = s->ctoken;
+    s->ctoken = NULL;
+    return ret;
+  }
+}
+
+/* Puts token into command stream. */
+int put_token(command_stream_t s, token_t t) 
+{
+  if (s->ctoken) 
+  {
+    return 1;
+  }
+  else
+  {
+    s->ctoken = t;
+    return 0;
+  }
+}
+
 /* Allocate memory for token. */
 token_t create_token(void)
 {
-  token_t new_token;
-  new_token = checked_malloc(sizeof(struct TOKEN));
+  token_t new_token = checked_malloc(sizeof(struct TOKEN));
   new_token->type = 0;
   new_token->token_name = NULL;
   return new_token;
@@ -127,24 +163,27 @@ token_t create_token(void)
 token_t fill_token(command_stream_t s, int c)
 {
   token_t new_token = create_token();
+  int lookahead;
 
   switch(c) {
     case ';':
       new_token->type = SEMICOLON;
       break;
     case '|':
-      if(get_check(s) == '|')
+      lookahead = get(s);
+      if(lookahead == '|')
       {
         new_token->type = OR;
       }
       else
       {
-        unget_check(s, get_check(s));
+        put(s, lookahead);
         new_token->type = PIPELINE;
       }
       break;
     case '&':
-      if(get_check(s) == '&')
+      lookahead = get(s);
+      if(lookahead == '&')
       {
         new_token->type = AND;
       }
@@ -168,24 +207,34 @@ token_t fill_token(command_stream_t s, int c)
   return new_token;
 }
 
-/* Distinguish between grammar of syntax with enum. Return 0 for WORD and 1 for SPECIAL TOKEN. */
+/* Distinguish between grammar of syntax with enum. */
 int getGrammar(int c)
 {
   if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || isdigit(c) || c == '!' || c == '%' || c == '+' || c == ',' || c == '-' || c == '.' || c == '/' || c == ':' || c == '@' || c == '^' || c == '_')
+  {
     return 0;
-  else if(c == ';' || c == '|' || c == '&' || c == '(' || c == ')' || c == '<' || c == '>' || c == '\n')
+  }
+  else if(c == ';' || c == '|' || c == '&' || c == '(' || c == ')' || c == '<' || c == '>')
+  {
     return 1;
+  }
   else
+  {
     error(1, 0, "%d: Syntax Error - Unsupported characters.", line);
+  }
+  return 1;
 }
 
-int consume_whitespace(command_stream_t s)
+/* Removes whitespace because whitespace is not part of token definition. */
+int remove_whitespace(command_stream_t s)
 {
   int curr;
+  int prevcurr;
   int track_newline = 0;
 
-  while((curr = get_check(s) != EOF) && (isspace(curr)))
+  while((curr = get(s)) != EOF && isspace(curr))
   {
+    prevcurr = curr;
     if(curr == '\n')
     {
       line++;
@@ -200,42 +249,52 @@ int consume_whitespace(command_stream_t s)
 
   if(curr == '#')
   {
-    while(((curr = get_check(s)) != '\n') && (curr != EOF))
+    if(getGrammar(prevcurr) != WORD)
     {
+      do
+      {
+        curr = get(s);
+      }
+      while(curr != EOF && curr != '\n');
+
       line++;
       return '\n';
     }
+    else
+    {
+      error(1, 0, "%d: Syntax Error - Not a valid comment.", line);
+    }
   }
-  //else
-  //{
-  //    error(1, 0, "%d: Syntax Error - Not a valid comment.", line);
-  //}
 
-  unget_check(s, curr);
+  put(s, curr);
   return track_newline;
 }
 
+/* Looks at the next section of the stream and does the appropriate actions. */
 token_t get_next_token(command_stream_t s)
 {
-  char* word_array;
-  char* temp;
+  char* word_array = '\0';
+  char* temp = '\0';
   size_t size = 0;
 
-  int curr = consume_whitespace(s);
+  int curr = remove_whitespace(s);
  
   if(curr == '\n')
   {
     token_t new_token = create_token();
     new_token->type = NEWLINE;
+    return new_token;
   }
   
   if(curr == EOF)
   {
     token_t new_token = create_token();
     new_token->type = END_FILE;
+    return new_token;
   }
 
-  curr = get_check(s);
+  curr = get(s);
+  
   if(getGrammar(curr))
   {
     return fill_token(s, curr);
@@ -245,25 +304,26 @@ token_t get_next_token(command_stream_t s)
   new_token->type = WORD;
 
   word_array = checked_malloc(sizeof(char) * 5);
+  temp = word_array;
   *(temp++) = curr;
   size++;
   
-  for(;;)
+  while(1)
   {
-    curr = get_check(s);
+    curr = get(s);
     
     if(curr == EOF || isspace(curr) || getGrammar(curr))
     {
       if(curr != EOF)
       {
-        unget_check(s, curr);
+        put(s, curr);
       }
       *temp = '\0';
       new_token->token_name = word_array;
       return new_token;
     }
     
-    *(temp++) == curr;
+    *(temp++) = curr;
     size++;
 
     if((size % 5) == 0) 
@@ -271,6 +331,12 @@ token_t get_next_token(command_stream_t s)
       word_array = checked_realloc(word_array, size+5);
     }
   }
-  
+
   return new_token;
 }
+
+/*************************************************
+ *************************************************
+ ******* End of TOKENIZER Implementation *********
+ *************************************************
+ *************************************************/
